e528731a0a6e45d96abcf0204679e9be213d8c9f
Anthony <amg568@drexel.edu>
2020-08-11 17:07:05 -0400

    Added Firebase to Project

--- final_project/src/index.js
+++ final_project/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const config = {
+
+
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
