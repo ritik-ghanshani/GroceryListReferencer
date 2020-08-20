ae61fd9ff05ebc020126927b0365efd9efdabeaf
samehabedin <sa3473@drexel.edu>
2020-08-19 23:22:13 -0400

    added more notes

--- final_project/src/server.js
+++ final_project/src/server.js
@@ -22,23 +22,37 @@ const config = {
 };
 
 firebase.initializeApp(config);
-//all will return json objects
-//app.post("/newUser"
+// all will return json objects
+// Normal JSON Object =  json object client sends server is { product : quantity} ({key:value})
 // node modules crypto to create unique ids
-// app.get("/getUser", function(req,res) {
-// query string
-// return names of already existing grocuery lists
-// app.get("/retrieveGroceryList", 
-// query string
-// return specific grocery list based on name
-// app.post("/createGroceryList", 
-// body of fetch
-// app.get("/checkAvail"
-// query string
-// app.delete("/deleteList
-// query string
-// app.put?(updateList)
-// fetch body 
+//
+// app.post("/newUser?user=xxx"
+// return status
+//
+// app.get("/getUserLists?user=xxx", function(req,res) {
+// return status
+// return names of already existing grocery lists
+//
+// app.get("/retrieveGroceryList?user=xxx&groceryList=yyy", 
+// return specific grocery list based on name and user
+// returns Normal JSON Object
+//
+//
+// app.post("/createGroceryList?user=xxx&groceryList=yyy", 
+// body of fetch send Normal JSON Object
+// send status for server res
+//
+// app.get("/checkAvail?user=xxx&groceryList=yyy"
+// return json object {product: true/false} based on availability
+//
+//
+// app.delete("/deleteList?user=xxx&groceryList=yyy"
+// send status back
+//
+// app.put("/updateList?user=xxx&groceryList=yyy")
+// fetch body Normal JSON Object
+// send status back 
+//
 // use date stamp to keep track of grocery list
 
 var ref =  firebase.database().ref();
