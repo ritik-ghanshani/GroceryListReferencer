0b45b0789778e9c0a567ed2a5da9c4454c918e2d
samehabedin <sa3473@drexel.edu>
2020-08-22 11:09:26 -0400

    Created newUser Route

--- final_project/src/server.js
+++ final_project/src/server.js
@@ -25,9 +25,39 @@ firebase.initializeApp(config);
 // authentication? extra, possible at the end
 // Normal JSON Object =  json object client sends server is { product : quantity} ({key:value})
 // node modules crypto to create unique ids
-//
-// app.post("/newUser?user=xxx"
-// return status
+// newUser?user=xxx
+app.post("/newUser", function(req,res) {
+    username = req.query.user;
+    if (!username) {
+        console.log("no username provided");
+        res.status(400);
+        res.json("error" : "No username provided");
+    }
+    var userRef = firebase.database().ref('users/' + username);
+    userRef.transaction( function(currentData) {
+       if (currentData == null) {
+            return  "placeHolderGroceryList";
+       }
+       else  {
+           console.log("User already exists!")
+           return;
+       }
+    }, function(error,committed,snapshot) {
+        if (error) {
+           res.status(500);
+           res.json({"error" : "Unknown Internal Server Error"});
+        }
+        else if ( !committed ) {
+           res.status(501)
+           res.json({"error" : "Username already taken"});
+        }
+        else {
+            console.log(username + " was added!");
+            res.send();
+        }
+    })
+    console.log(username)
+})
 //
 // app.get("/getUserLists?user=xxx", function(req,res) {
 // return status
@@ -41,6 +71,9 @@ firebase.initializeApp(config);
 // app.post("/createGroceryList?user=xxx&groceryList=yyy", 
 // body of fetch send Normal JSON Object
 // send status for server res
+app.post("/createGroceryList", function(req,res) {
+    username = req.query.user;
+})
 //
 // app.get("/checkAvail?user=xxx&groceryList=yyy"
 // return json object {product:[ true/false, numAvailableInGroceryStore]} based on availability
@@ -57,12 +90,13 @@ firebase.initializeApp(config);
 //
 // use date stamp to keep track of grocery list
 
+/*
 var ref =  firebase.database().ref();
 ref.once("value").then( function(snapshot) {
     let name = snapshot.child("hey").val();
     console.log(name);
 });
-
+*/
 
 
 app.listen(port, hostname, () => {
