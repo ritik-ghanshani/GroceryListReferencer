7e1f797c7ed687bb215f526146ea994ac928d21e
Anthony <amg568@drexel.edu>
2020-08-30 10:52:35 -0400

    working on login page

--- final_project/src/App.js
+++ final_project/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { NavBar } from './components/NavBar';
+import {LoginPage} from './components/LoginPage';
 //import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
-    return <NavBar />;
+    return (
+        
+        //<NavBar />
+        <LoginPage />
+
+    );
 }
 
 export default App;
---
+++ final_project/src/components/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class LoginPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h>Login Page</h>
+                <button>Login</button>
+            </div>
+          
+
+
+        )
+    }
+
+}
