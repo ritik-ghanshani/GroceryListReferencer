7974c1a2f121ff200bafb8480fe8f64114ec90a0
Anthony <amg568@drexel.edu>
2020-08-30 13:57:47 -0400

    added logic for login page

--- final_project/src/App.js
+++ final_project/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import {LoginPage} from './components/LoginPage';
 //import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
+    const [isLoggedIn, setLogIn] = useState(false);
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
+        //<div><NavBar /></div>
+        <div><LoginPage name={userName} pass={password}/></div>
         
-        //<NavBar />
-        <LoginPage />
 
     );
 }
--- final_project/src/components/LoginPage.js
+++ final_project/src/components/LoginPage.js
@@ -1,16 +1,73 @@
-import React from 'react';
+import React from "react";
+import axios from 'axios';
 
 export class LoginPage extends React.Component {
-    render() {
-        return (
-            <div>
-                <h>Login Page</h>
-                <button>Login</button>
-            </div>
-          
-
-
-        )
+    constructor(props) {
+        super(props);
+    
+    this.state = {
+        username: "",
+        password: "",
+        loginErrors:""
+    };
+    this.validateSubmit = this.validateSubmit.bind(this);
+    this.validateChange = this.validateChange.bind(this);
     }
+
+    validateChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    validateSubmit(event){
+        const {username, password} = this.state;
+        axios.post(
+            "/userSubmit", //this name is tentative
+            {
+                user: {
+                    username: username,
+                    password: password
+                }
+            }, {withCredentials: true}
+        ).then(response => {
+            if (response.data.logged_in) {
+                this.props.handleSuccesAuth(response.data);
+            }
+        })
+        .catch(error => {
+            console.log(`login error ${error}`);
+        });
+        event.preventDefault();
+    }
+    
+
+    render() {
+            return (
+                <div>
+                <h1>Login Page</h1>           
+                    <form>
+                        <input type="text" 
+                        name="username"
+                        placeholder="Name"
+                        value={this.state.username}
+                        onChange={this.validateChange}
+                        required 
+                        autoFocus
+                        />
+                        <br/>
+                        <input type="password" 
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.validateChange}
+                        required
+                        />
+                        <br/>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            );
+        }
 
 }
