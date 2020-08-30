f248a2ab430e2e5775e5ed13219c3c51c65fb87d
kevinkarnani <kevinkarnani@gmail.com>
2020-08-30 15:37:42 -0400

    eslint fixes

--- final_project/src/App.js
+++ final_project/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 //import { NavBar } from './components/NavBar';
-import {Login} from './components/Login';
+import { Login } from './components/Login';
 //import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
     return (
         //<div><NavBar /></div>
-        <div><Login/></div>
+        <div>
+            <Login />
+        </div>
     );
 }
 
--- final_project/src/components/Login.js
+++ final_project/src/components/Login.js
@@ -1,73 +1,75 @@
-import React from "react";
+import React, { Component } from 'react';
 import axios from 'axios';
 
-export class Login extends React.Component {
+export class Login extends Component {
     constructor(props) {
         super(props);
-    this.state = {
-        username: "",
-        password: "",
-        loginErrors:""
-    };
-    this.validateSubmit = this.validateSubmit.bind(this);
-    this.validateChange = this.validateChange.bind(this);
+        this.state = {
+            username: '',
+            password: '',
+            loginErrors: '',
+        };
+        this.validateSubmit = this.validateSubmit.bind(this);
+        this.validateChange = this.validateChange.bind(this);
     }
 
-    validateChange(event){
+    validateChange(event) {
         this.setState({
-            [event.target.name]: event.target.value
+            [event.target.name]: event.target.value,
         });
     }
 
-    validateSubmit(event){
-        const {username, password} = this.state;
-        axios.post(
-            "/userSubmit", //this name is tentative
-            {
-                user: {
-                    username: username,
-                    password: password
+    validateSubmit(event) {
+        const { username, password } = this.state;
+        axios
+            .post(
+                '/userSubmit',
+                {
+                    user: {
+                        username: username,
+                        password: password,
+                    },
+                },
+                { withCredentials: true }
+            )
+            .then((response) => {
+                if (response.data.logged_in) {
+                    this.props.handleSuccesAuth(response.data);
                 }
-            }, {withCredentials: true}
-        ).then(response => {
-            if (response.data.logged_in) {
-                this.props.handleSuccesAuth(response.data);
-            }
-        })
-        .catch(error => {
-            console.log(`login error ${error}`);
-        });
+            })
+            .catch((error) => {
+                console.log(`login error ${error}`);
+            });
         event.preventDefault();
     }
-    
-    
 
     render() {
-            return (
-                <div>
-                <h1>Login Page</h1>           
-                    <form>
-                        <input type="text" 
+        return (
+            <div>
+                <h1>Login Page</h1>
+                <form>
+                    <input
+                        type="text"
                         name="username"
                         placeholder="Name"
                         value={this.state.username}
                         onChange={this.validateChange}
-                        required 
+                        required
                         autoFocus
-                        />
-                        <br/>
-                        <input type="password" 
+                    />
+                    <br />
+                    <input
+                        type="password"
                         name="password"
                         placeholder="Password"
                         value={this.state.password}
                         onChange={this.validateChange}
                         required
-                        />
-                        <br/>
-                        <button type="submit" >Login</button>
-                    </form>
-                </div>
-            );
-        }
-
+                    />
+                    <br />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        );
+    }
 }
