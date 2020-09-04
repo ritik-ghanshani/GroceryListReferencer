27c1af2019e61110e1055e051dba9573eb79aa5b
samehabedin <sa3473@drexel.edu>
2020-09-04 11:00:31 -0400

    added firebase configs to .env.json file and removed firebase config from src/index.js

--- final_project/functions/index.js
+++ final_project/functions/index.js
@@ -13,19 +13,18 @@ const app = express();
 //const cors = require('cors');
 
 const firebase = require('firebase');
-const port = 3000;
-const hostname = 'localhost';
+const firebaseEnvFile = require('../.env.json')
 app.use(express.json());
 
 const config = {
-    apiKey: 'AIzaSyD1KuT744S0uB27hGWm35J638_-O5fYW08',
-    authDomain: 'fir-9fcf5.firebaseapp.com',
-    databaseURL: 'https://fir-9fcf5.firebaseio.com/',
-    projectId: 'fir-9fcf5',
-    storageBucket: 'fir-9fcf5.appspot.com',
-    messagingSenderId: '935434602441',
-    appId: '1:935434602441:web:a57456a42b9237ee600a18',
-    measurementId: 'G-G0QD9N03RC',
+    apiKey: firebaseEnvFile['API_KEY'],
+    authDomain: firebaseEnvFile['AUTH_DOMAIN'],
+    databaseURL: firebaseEnvFile['DATABASE_URL'],
+    projectId: firebaseEnvFile['PROJECT_ID'],
+    storageBucket: firebaseEnvFile['STORAGE_BUCKET'],
+    messagingSenderId: firebaseEnvFile['MESSAGING_SENDER_ID'],
+    appId: firebaseEnvFile['APP_ID'], 
+    measurementId: firebaseEnvFile['MEASUREMENT_ID'],
 };
 
 firebase.initializeApp(config);
--- final_project/src/index.js
+++ final_project/src/index.js
@@ -3,22 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
 
-const config = {
-    apiKey: 'AIzaSyD1KuT744S0uB27hGWm35J638_-O5fYW08',
-    authDomain: 'fir-9fcf5.firebaseapp.com',
-    databaseURL: 'https://fir-9fcf5.firebaseio.com/',
-    projectId: 'fir-9fcf5',
-    storageBucket: 'fir-9fcf5.appspot.com',
-    messagingSenderId: '935434602441',
-    appId: '1:935434602441:web:a57456a42b9237ee600a18',
-    measurementId: 'G-G0QD9N03RC',
-};
-
-firebase.initializeApp(config);
 
 ReactDOM.render(
     <React.StrictMode>
